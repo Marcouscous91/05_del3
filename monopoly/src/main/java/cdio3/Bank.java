1e31c17efff54d37465b3b2f9c52d90686bbd793
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-11-12 20:45:32 +0100

    Made Bank class prototype, removed abstract part

--- monopoly/src/main/java/cdio3/Bank.java
+++ monopoly/src/main/java/cdio3/Bank.java
@@ -1,10 +1,32 @@
-public class Bank extends
-abstract class Actor {
-public void payToBank(fromPlayer.amount){
+public class Bank {
 
-        public void payToBank(Player fromPlayer, int amount) {
-            int pay = abletopPay(fromPlayer, amount);
+    public void payToBank(Player fromPlayer, int amount) {
+        int pay = abletoPay(fromPlayer, amount);
 
+        /*
+         * if (player.getBalance() <= 0 || amount <= 0) {
+         * return 0;
+         * }
+         * if (player.getBalance() - amount < 0) {
+         * return player.getBalance();
+         * }
+         * return amount;
+         */
+    }
+
+    // I think this belongs in "Player class"
+    public void payToPlayer(Player fromPlayer, Player toPlayer, int amount) {
+
+    }
+
+    private int ableToPay(Player player, int amount) {
+        if (player.getBalance() <= 0 || amount <= 0) {
+            return 0;
         }
+        if (player.getBalance() - amount < 0) {
+
+            return player.getBalance();
+        }
+        return amount;
     }
 }
