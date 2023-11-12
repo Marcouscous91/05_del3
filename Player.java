caf5d276b480388b77d60ddce51ee9ddbe5b4d9c
S235119 <S235119@dtu.dk>
2023-11-12 18:32:05 +0100

    Player fra CDIO3 med mindre ændringer

---
+++ Player.java
@@ -0,0 +1,57 @@
+package cdio3;
+
+class Player {
+
+    private Account account;
+    private int age;
+
+    // Constructor for a new Player
+    public Player(String age) {
+        this.age = age;
+        createAccount();
+    }
+
+    private void createAccount() {
+        this.account = new Account();
+    }
+
+    // Method to get a playerAge and return it
+    public int getAge() {
+        return age;
+    }
+
+    // Method to set/change an animalToken
+    public void setAnimalToken(String animalToken) {
+        this.animalToken = animalToken;
+    }
+
+    //Don't know if these last ones should be kept, or what to call them, so i won't change them right now
+    //Might make sense to keep them as almost the same, but i'm not quite sure
+    public void setBalance(double money){
+        account.setBalance(money);
+    }
+
+    // Method to get the players balance and return it
+    public double getBalance() {
+        return account.getBalance();
+
+    }
+
+    // Method to add money/points to Player accoutn
+    public void addSum(double money) {
+        account.addSum(money);
+
+    }
+
+    // Method to subtract money/sum from Players account
+    public void subtractSum(double money) {
+        account.subtractSum(money);
+
+    }
+
+    // Method to print Player info
+    public void displayPlayerInfo() {
+        System.out.println("AnimalToken: " + animalToken);
+        System.out.println("Money: " + account.getBalance());
+    }
+}
