4c93cf33233b7ea122754f352a24bf98b84215ed
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-11-14 08:26:33 +0100

    Added dokumentation

--- monopoly/src/main/java/cdio3/Bank.java
+++ monopoly/src/main/java/cdio3/Bank.java
@@ -1,24 +1,16 @@
 public class Bank {
-
+// Constructor to pay bank "amount"
     public void payToBank(Player fromPlayer, int amount) {
         int pay = abletoPay(fromPlayer, amount);
 
-        /*
-         * if (player.getBalance() <= 0 || amount <= 0) {
-         * return 0;
-         * }
-         * if (player.getBalance() - amount < 0) {
-         * return player.getBalance();
-         * }
-         * return amount;
-         */
     }
+    //Constructor to pay from "Player" to "Player"
 
     // I think this belongs in "Player class"
     public void payToPlayer(Player fromPlayer, Player toPlayer, int amount) {
 
     }
-
+// Checks if player is abletopay amount
     private int ableToPay(Player player, int amount) {
         if (player.getBalance() <= 0 || amount <= 0) {
             return 0;
