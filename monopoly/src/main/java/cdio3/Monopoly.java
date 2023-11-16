d84669cc90d5a71be789ea5faded8c1b658713b5
Rebecca Moss <moss.rebecca.10@gmail.com>
2023-11-16 13:35:36 +0100

    Spilet køre kun med mellem 2 og 4 spiller

--- monopoly/src/main/java/cdio3/Monopoly.java
+++ monopoly/src/main/java/cdio3/Monopoly.java
@@ -8,6 +8,11 @@ class Monopoly {
         System.out.println("Enter number of players:");
         String command = scanner.nextLine();
         int numberOfPlayers = Integer.parseInt(command);
+        while (numberOfPlayers < 2 || numberOfPlayers > 4) {
+            System.out.println("you have to write a number between 2 and 4");
+            command = scanner.nextLine();
+            numberOfPlayers = Integer.parseInt(command);
+        }
         Game monopoly = new Game(numberOfPlayers);
         monopoly.play();
     }
