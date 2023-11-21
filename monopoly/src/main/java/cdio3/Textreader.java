e2bbcf9a6f5c6e9369fa7c2ab09faf598982593e
Marcus Lemser <mle91@protonmail.com>
2023-11-21 12:23:09 +0100

    More documentation

--- monopoly/src/main/java/cdio3/Textreader.java
+++ monopoly/src/main/java/cdio3/Textreader.java
@@ -10,6 +10,7 @@ class Textreader {
     private static String[] fullFile;
 
     public static void loadFile(String filename){
+        // Creates the absolute path name of the file
         Path currentRelPath = Paths.get("src\\main\\java\\cdio3\\" + filename);
         String pathName = currentRelPath.toAbsolutePath().toString();
         File file = new File(pathName);
@@ -25,6 +26,12 @@ class Textreader {
                         Integer.parseInt(l.toCharArray()[0]+"");
                         lines++;
                     }
+                /*
+                 * If the first character of the curent line is not an int, and thus can't be parsed
+                 * as an integer, Exception e will be thrown, and caught, and then the loop will
+                 * continue. This means the loop will only count amount of lines, of text which will 
+                 * be printed
+                 */
                 } catch (IllegalArgumentException e) {
                     continue;
                 }                          
@@ -41,6 +48,18 @@ class Textreader {
             for (int i = 0; scanner.hasNextLine(); i++) {
                 int index;
                 String l = scanner.nextLine();
+                /*
+                 * Splits the current line of text for every ':', and creates an array of Strings,
+                 * seperated by each ':'. First element of this array will be parsed as Integer in 
+                 * 'index', and the second element of the array, will be stored in the array 
+                 * 'fullFile' at 'index' of the first element.
+                 * 
+                 * If the first element is not a number and thus can't be parsed, an exception will
+                 * be thrown and caught, and then the loop continues.
+                 * 
+                 * This way all lines to be printed will be stored in the array 'fullFile', at the index
+                 * indicated in the .txt file
+                 */
                 try{
                     String[] lineSplitted = l.split(":");
                     index = Integer.parseInt(lineSplitted[0]);
@@ -49,14 +68,20 @@ class Textreader {
                     continue;
                 }
             }
-
         } catch (IOException e) {
             System.err.println("Error reading file: " + filename);
         }
     }
 
+    /*
+     * Returns the string to be printed
+     * 
+     * Param:   lineNumber: the line from the .txt file to be printed
+     * 
+     * Return:  textLineToPrint:    the String at position 'lineNumer'
+     */
     public static String getTextLineToPrint(int lineNumber) {
-        String getTextLineToPrint = fullFile[lineNumber];
-        return getTextLineToPrint;
+        String textLineToPrint = fullFile[lineNumber];
+        return textLineToPrint;
     }
 }
